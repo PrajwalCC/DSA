bf427d98569246063cad3dca7ac310779163c330
PrajwalCC <b220035@iiit-bh.ac.in>
2022-12-08 00:47:51 +0530

    ok

---
+++ 7.DP/1_2_fibonacci_bottom_up-tabulation.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int fib(int n, vector<int>& dp){
+    dp.push_back(1);
+    dp.push_back(1);
+    // dp[1]=1;
+    for(int i=2; i<=n; i++){
+        int temp=dp[i-1]+dp[i-2];
+        dp.push_back(temp);
+    }
+
+    return dp[n];
+}
+int main(){
+    int n = 4;
+    vector<int> dp;
+    cout<<fib(n,dp);
+    return 0;
+}
